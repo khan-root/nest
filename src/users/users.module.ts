ddcb8858141ff0642962259b7197a7d08da353df
Sarmad Faizan <sarmadf49@gmail.com>
2025-07-16 09:07:53 +0500

    simple middleware implement

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { UsersMiddleware } from './users.middleware';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService]
 })
-export class UsersModule {}
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+      consumer.apply(UsersMiddleware).forRoutes('users')
+  }
+}
