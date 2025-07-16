88ffe9075a5aee7980ed5c033cad58acde687fd5
Sarmad Faizan <sarmadf49@gmail.com>
2025-07-16 10:44:16 +0500

    connect to mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,30 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { UsersController } from './users/users.controller'
+import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const uri = configService.get<string>('MONGO_URI');
+        return {
+          uri,
+          useNewUrlParser: true,
+        };
+      },
+      inject: [ConfigService],
+    }),
+
+    UsersModule,
+  ],
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
 })
